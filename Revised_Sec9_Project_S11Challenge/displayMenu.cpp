090c90dcebcf5a985b012229bf5dabbdfd0e8d35
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-08-01 22:07:17 -0500

    Create displayMenu.cpp

---
+++ Revised_Sec9_Project_S11Challenge/displayMenu.cpp
@@ -0,0 +1,9 @@
+#include "include.h"
+void displayMenu(){
+    std::cout << "P - Print numbers\n" 
+              << "A - Add a number\n" 
+              << "M - Display mean of the numbers\n"  
+              << "S - Display the smallest number\n" 
+              << "L - Display the largest number\n" 
+              << "Q - Quit\n\n";
+}
