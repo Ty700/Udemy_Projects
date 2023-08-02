775014ab2cfa753f3ac932514ab93fa659582eb3
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-08-01 22:37:18 -0500

    Update Sec11_main.cpp

--- Revised_Sec9_Project_S11Challenge/Sec11_main.cpp
+++ Revised_Sec9_Project_S11Challenge/Sec11_main.cpp
@@ -1,4 +1,4 @@
-#include "include.h"
+#include "preprocessor_directives.h"
 #include "displayMenu.cpp"
 #include "menuAction.cpp"
 
@@ -6,8 +6,13 @@ int main(){
     std::vector<int> list;
     char choice;
     do{
+        //Displays the menu to the user
         displayMenu();
-        std::cin >> choice;
+        
+        //Gets the menu action the user wants to preform
+        getChoice(choice); //in "menuActionFunctions.cpp"
+
+        //Completes the action the user wants
         menuAction(choice, list);
     }while(choice != 'Q' && choice != 'q');
 
