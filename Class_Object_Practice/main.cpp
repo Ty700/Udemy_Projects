129ee3045dfbc627dd3c87ed3714cead266be32d
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-08-04 10:56:51 -0500

    Create main.cpp

---
+++ Class_Object_Practice/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+#include "Player.cpp"
+
+int main(){
+    //Stack obj
+    Player Tyler;
+    Tyler.setName("Tyler");
+    Tyler.talk("Scary Ghoul spotted!\n");
+
+    //Heap obj
+    Player *enemy = new Player;
+    enemy->setName("Ghoul");
+    enemy->setHP(25);
+    
+    //need to use -> for heap. -> = *(enemy).method();
+    enemy->talk("You're all mine!\n");
+
+    enemy->setHP(enemy->getHP() - 25);
+
+    //Did an "attack" on the enemy
+    std::cout << Tyler.getName() << " attacked " << enemy->getName() << " and now the enemy's health is: " << enemy->getHP() << std::endl;
+
+    //will always be 0, but later I will add a random number 0-25 that will be the "attack" on the enemy and the health will not always = 0.
+    if(enemy->getHP() <= 0){
+        std::cout << "Ghoul collapeses and appears dead.";
+    } else {
+        std::cout << "Is that all you got?";
+    }
+
+}
