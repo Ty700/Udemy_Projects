972c9a60f39aa4b39f1afe9a6835a2d3187ffc11
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-08-05 17:48:35 -0500

    Update main.cpp
    Simplified main for the future of the project.

--- Hero vs Villian Game/main.cpp
+++ Hero vs Villian Game/main.cpp
@@ -1,24 +1,7 @@
 #include "Player.h"
+#include "Game.h"
 
 int main(){
-    
-    Player *Hero = new Player{"Tyler"};
-    Player *Enemy = new Player{"Ghoul"};
-
-    Hero->talk("Scary Ghoul spotted!\n");
-    Enemy->talk("You're all mine!\n");
-
-    do{
-    _sleep(1000);
-    //Hero did an "attack" on the enemy
-    Hero->attack(*Enemy);
-        if(Enemy->getHP() < 0){
-            break;
-        }
-    _sleep(1000);
-    //Enemy's turn
-    Enemy->attack(*Hero);
-    }while(Hero->getHP() > 0 && Enemy->getHP() > 0);
-    
-    delete Hero, Enemy;
+    //is this to simple?? IDK. Did this for the future of the project really. 
+    playGame();
 }
