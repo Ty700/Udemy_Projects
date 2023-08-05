301ba877e3fb4a57ff6d59b9b50628dc0b2742ee
Tyler S <118336783+Ty700@users.noreply.github.com>
2023-08-05 17:49:00 -0500

    Create Game.cpp

---
+++ Hero vs Villian Game/Game.cpp
@@ -0,0 +1,28 @@
+#include "Player.h"
+#include "Game.h" 
+
+void playGame(){
+    Player *Hero = new Player{"Tyler"};
+    Player *Enemy = new Player{"Ghoul"};
+
+    Hero->talk("Scary Ghoul spotted!\n");
+    Enemy->talk("You're all mine!\n");
+
+    do{
+
+    _sleep(1000);
+    //Hero attacks enemy
+    Hero->attack(*Enemy);
+
+    //checks if enemy is dead
+        if(Enemy->getHP() < 0){
+            break;
+        }
+
+    _sleep(1000);
+    //Enemy's turn
+    Enemy->attack(*Hero);
+    }while(Hero->getHP() > 0 && Enemy->getHP() > 0);
+    
+    delete Hero, Enemy;
+}
